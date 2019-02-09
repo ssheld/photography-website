7c4d681d31ceb28da54fc80f2686415d5df7c4e7
Stephen <sheldon2521@gmail.com>
2019-02-09 12:52:28 -0500

    Created unitednations gallery page

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -8,6 +8,24 @@
 $(document).ready(function(){
     
     /* Make our fixed header transparent */
-    $(".header").css('background-color', 'rgb(17,17,17)');
+    $('.header').css('background-color', 'rgb(17,17,17)');
+
+    console.log("MAKING A NEW GALLERY");
+    /* Initialize light gallery */
+    $('#aniimated-thumbnails').lightGallery({
+        mode: 'lg-slide',
+        // cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        height: '100%',
+        width: '100%',
+        getCaptionFromTitleOrAlt: false,
+        download: false,
+        zoom: false,
+        autoplay: false,
+        progressBar: false,
+        autoplayControls: false
+        // Enable keyboard navigation
+        // KeyPress: 'true',
+        // thumbnail: 'true'
+    });
     
 });
