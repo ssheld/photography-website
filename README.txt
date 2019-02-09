cd90123fb5d3a5947bed78d135dfb1fd8c8119f2
Stephen <sheldon2521@gmail.com>
2019-02-09 13:20:31 -0500

    Added minified jquery file

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 This Website Uses the following JQuery Libraries.
 http://idangero.us/swiper/
 https://github.com/bfred-it/object-fit-images
+https://github.com/sachinchoolur/lightGallery
--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,20 +1,13 @@
 
 
-
-
-
-
-
 $(document).ready(function(){
     
     /* Make our fixed header transparent */
     $('.header').css('background-color', 'rgb(17,17,17)');
 
-    console.log("MAKING A NEW GALLERY");
     /* Initialize light gallery */
     $('#aniimated-thumbnails').lightGallery({
         mode: 'lg-slide',
-        // cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
         height: '100%',
         width: '100%',
         getCaptionFromTitleOrAlt: false,
@@ -22,10 +15,8 @@ $(document).ready(function(){
         zoom: false,
         autoplay: false,
         progressBar: false,
-        autoplayControls: false
-        // Enable keyboard navigation
-        // KeyPress: 'true',
-        // thumbnail: 'true'
+        autoplayControls: false,
+    
     });
     
 });
